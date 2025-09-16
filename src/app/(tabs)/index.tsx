f3480100cd717eb81db258724417ff4c506e607a
AlePan <darenagel@hotmail.it>
2025-09-16 07:02:31 +0200

    configure style and data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import Colors from '../../constants/Colors';
+import products from '../../../assets/data/products';
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: '600',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+  }
 });
