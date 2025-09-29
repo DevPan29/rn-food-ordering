2d18a8d76140832a85d5530c3e4ee1268c6f22ac
AlePan <darenagel@hotmail.it>
2025-09-29 12:42:04 +0200

    image picker

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -33,20 +33,11 @@ const ProductDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: product?.name }}/>
       <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image}/>
-      <Text >Select size</Text>
-      <View style={styles.sizes}>
-      {sizes.map(size => 
-        <Pressable key={size} 
-            onPress={() => setSelectedSize(size)} 
-            style={[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro' : 'white'}]} >
-          <Text 
-            style={[styles.sizeText, { color: selectedSize === size ? 'black' : 'grey'}]}>{size}</Text>
-        </Pressable>
-      )}
-      </View>
+      
 
+      <Text style={styles.title}>${product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-      <Button text="Add to cart" onPress={addToCart} />
+      {/* <Button text="Add to cart" onPress={addToCart} /> */}
     </View>
   )
 }
@@ -59,30 +50,16 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
   image: {
     width: '100%',
     aspectRatio: 1
   },
   price: {
     fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 'auto'
-  },
-  sizes : {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginVertical: 10
-  },
-  size: {
-    backgroundColor: 'gainsboro',
-    width: 50,
-    aspectRatio: 1,
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  sizeText: {
-    fontSize: 20,
-    fontWeight: '500'
-  },
+    fontWeight: 'bold'
+  }
 })
