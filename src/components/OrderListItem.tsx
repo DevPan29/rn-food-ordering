d5902ea48b4e1c167cb4fe1fcc162c84d070bd57
AlePan <darenagel@hotmail.it>
2025-10-07 07:11:12 +0200

    Assignmens 2: Orders

--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,61 +1,52 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Order } from '@/types'
-import Colors from '@/constants/Colors'
-import { Link, useSegments } from 'expo-router'
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import React from 'react';
+import { Order } from '../types';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
+import { Link, useSegments } from 'expo-router';
 
-type OrderListItemProp = {
-    order: Order
-}
+dayjs.extend(relativeTime);
 
-const OrderListItem = ({order}: OrderListItemProp ) => {
+type OrderListItemProps = {
+  order: Order;
+};
+
+const OrderListItem = ({ order }: OrderListItemProps) => {
   const segments = useSegments();
-
-  const getHoursAgo = () => {
-    const orderDate = new Date(order.created_at);
-    const now = new Date();
-    const diffInMs = now.getTime() - orderDate.getTime();
-    const diffInHours = Math.round(diffInMs / (1000 * 60 * 60));
-    return diffInHours;
-  }
-
-  const hoursAgo = getHoursAgo();
 
   return (
     <Link href={`/${segments[0]}/orders/${order.id}`} asChild>
-        <Pressable style={styles.container}>
-            <View>
-                <Text style={styles.title}>Order #{order.id}</Text>
-                <Text style={styles.creationDate}>{hoursAgo} hours ago</Text>
-            </View>
-            <Text style={styles.status}>{order.status}</Text>
-        </Pressable>
-    </Link>
-  )
-}
+      <Pressable style={styles.container}>
+        <View>
+          <Text style={styles.title}>Order #{order.id}</Text>
+          <Text style={styles.time}>{dayjs(order.created_at).fromNow()}</Text>
+        </View>
 
-export default OrderListItem
+        <Text style={styles.status}>{order.status}</Text>
+      </Pressable>
+    </Link>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-        borderRadius: 10,
-        flex: 1,
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        alignItems: 'center',
-      },
-      title: {
-        fontSize: 18,
-        fontWeight: 'bold'
-      },
-      creationDate: {
-        color: 'gray',
-        fontSize: 16,
-      },
-      status: {
-        fontSize: 16,
-      }
-})
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  time: {
+    color: 'gray',
+  },
+  status: {
+    fontWeight: '500',
+  },
+});
+
+export default OrderListItem;
