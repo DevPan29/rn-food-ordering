9a9db7d2acdd247a4efd61e5e9e18e501828935b
AlePan <darenagel@hotmail.it>
2025-10-15 06:37:53 +0200

    Authentication flow completed

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -2,19 +2,29 @@ import { supabase } from "@/lib/supabase";
 import { Session } from "@supabase/supabase-js";
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 
+// added this type to avoid TS errors on 'group' property
+type Profile = {
+  [key: string]: any;
+}
+
 type AuthData = {
     session: Session | null;
     loading: boolean;
+    profile: any;
+    isAdmin: boolean;
  }
 
 const AuthContext = createContext<AuthData>({
     session: null,
     loading: true,
+    profile: null,
+    isAdmin: false
 });
 
 export default function AuthProvider({children}: PropsWithChildren) {
     
     const [session, setSession] = useState<Session | null>(null);
+    const [profile, setProfile] = useState<Profile | null>(null);
     const [loading, setLoading] = useState(true);
     console.log(`AuthProvider session: ${JSON.stringify(session?.user?.email)}`);
 
@@ -22,8 +32,19 @@ export default function AuthProvider({children}: PropsWithChildren) {
         // check auth state
         // fetch user session from supabase
         const fetchSession = async () => {
-            const { data } = await supabase.auth.getSession()
-            setSession(data.session);
+            const { data: { session } } = await supabase.auth.getSession()
+            setSession(session);
+
+            if (session) {
+                // fetch profile
+                const { data } = await supabase
+                    .from('profiles')
+                    .select('*')
+                    .eq('id', session.user.id)
+                    .single();
+                setProfile(data || null);
+            }
+
             setLoading(false);
         };
         fetchSession();
@@ -32,8 +53,10 @@ export default function AuthProvider({children}: PropsWithChildren) {
         });
     }, []);
 
+    console.log(`profile: ${JSON.stringify(profile)}`);
+
     return (
-        <AuthContext.Provider value={{session, loading}}>
+        <AuthContext.Provider value={{session, loading, profile, isAdmin: profile?.group === 'ADMIN'}}>
             {children}
         </AuthContext.Provider>
     )
