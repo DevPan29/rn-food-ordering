034754881301367155dddaaacda25921a027b421
AlePan <darenagel@hotmail.it>
2025-10-16 14:24:13 +0200

    first fetching from supabase

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,8 +1,22 @@
 import { View, FlatList } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
+import { useEffect } from 'react';
+import { supabase } from '@/lib/supabase';
 
 export default function MenuScreen() {
+
+  useEffect(() => {
+    console.log('MenuScreen index.tsx rendered');
+    const fetchProducts = async () => {
+      const {data, error} = await supabase.from('products').select('*');
+      console.log('Fetching products error: ', error);
+      console.log('Fetching products data: ', data);
+    } 
+    fetchProducts();
+  }, [])
+
+
   return (
   <View>
       <FlatList 
